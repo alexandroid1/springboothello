8a58f04b0a0f0db3ec2d25dcd1c8ade87ad515cd
Alexksandr <archangel7017@gmail.com>
2017-10-20 18:45:06 +0300

    main js

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -1,11 +1,11 @@
 var app = angular.module("springboothello",[]);
 
 app.controller("AppCtrl", function($scope){
-    $scope.websites = [{
-        iconImageUrl: '',
-        id: 'stackoverflow',
-        website: 'stackoverflow.com',
-        title: 'stackowerflow website',
-        description: 'stackowerflow description'
-    }];
+    $scope.websites = [];
+
+/*    $http.get('http://localhost:8090/api/stackoverflow').success(function(data) {
+        $scope.websites = data;
+    });*/
+
+    $http.get('http://localhost:8090/api/stackoverflow').then(function (response) { $scope.websites=response.data; });
 });
